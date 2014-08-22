78c9428ab33244374ee304d7b2b420070c39968c
novopashinwm <novopashinwm@mail.ru>
2014-08-22 08:12:54 +0400

    Добавлен метод PlaceFigures - первоначальной расстановки фигур

--- ChessOOP/Board/Board.cs
+++ ChessOOP/Board/Board.cs
@@ -7,11 +7,24 @@ namespace ChessOOP
     //Класс доска
     public class Board
     {
-        public object[,] MyBoard ()
+        private Figure  [,] _board  = new Figure [8,8];
+
+        public Figure  [,] MyBoard 
         {
-            throw new NotImplementedException();
+            get {return _board ; }
         }
 
+        //Расставить фигуры
+        public void PlaceFigures()
+        { 
+           //Ставим пешки
+            for (int i = 0; i < 8; i++)
+            { 
+                
+            }
+        }
+
+        //Выбрать фигуру
         public Figure SelectFigure()
         {
             throw new NotImplementedException();
